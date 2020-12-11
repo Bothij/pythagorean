de0d099284ed77378f1d54ca40afb5f88866b788
Bothij <bothij@gmail.com>
2020-12-11 15:55:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+//void PowNumber1(double number_1, double number_2);
+//void PowNumber2(double number_1, double number_2);
+
+int main() {
+  
+  double number_1 = 3;
+  double number_2 = 4;
+  
+  std::cout << number_1 << "^2 + " << number_2 << "^2 = " << pow(number_1, 2) << " + " << pow(number_2, 2) << " = " << pow(number_1, 2) + pow(number_2, 2) << std::endl;
+  std::cout << pow(number_1, 2) + pow(number_2, 2) << " √ = " << sqrt(pow(number_1, 2) + pow(number_2, 2)) << std::endl; 
+}
+
+void PowNumber1(double number_1, double number_2) {
+
+  
+void PowNumber2(double number_1, double number_2);
